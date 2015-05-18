34374c83e0b306d676a9ac16933a3306e6edc2d9
Fintan Kearney <dev@Fintans-MacBook-Pro.local>
2015-05-19 00:35:15 +0100

    First commit: Basic Java output. Single dependency.

---
+++ README.md
@@ -0,0 +1,13 @@
+Build automation using Gradle for a simple Java Project.
+
+Initialize the wrapper scripts
+
+> gradle wrapper
+
+Build task
+
+> /gradlew build
+
+Run the project (must be using; apply plugin: 'application')
+
+> ./gradlew run
---
+++ src/main/java/gradlejava/GradleJava.java
@@ -0,0 +1,12 @@
+package gradlejava;
+
+import org.joda.time.LocalTime;
+
+public class GradleJava {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+
+    LocalTime currentTime = new LocalTime();
+    System.out.println("The current local time is: " + currentTime);	
+  }
+}
